ea679d71ba24d8a1312a91f53caf65956fdb764d
tatendanyahuku <tatendanyahuku@gmail.com>
2025-04-09 22:42:29 +0200

    Add telemedicine/users/admin.py 1744231348392

---
+++ telemedicine/users/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'user_type', 'is_staff')
+    list_filter = ('user_type', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'profile_picture', 'phone_number', 'address')}),
+        ('User Type', {'fields': ('user_type',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'user_type'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
